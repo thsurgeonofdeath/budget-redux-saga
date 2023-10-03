7609b73efb6a2956d70b18c03fd0dd6f47842060
thsurgeonofdeath <SELKADI@pca.ma>
2023-10-03 16:38:40 +0100

    hooks and managing state

--- src/App.js
+++ src/App.js
@@ -3,24 +3,117 @@ import "./App.css";
 import MainHeader from "./components/MainHeader";
 import DisplayBalance from "./components/DisplayBalance";
 import DisplayBalances from "./components/DisplayBalances";
-import EntryLine from "./components/EntryLine";
 import NewEntryForm from "./components/NewEntryForm";
+import { useEffect, useState } from "react";
+import EntryLines from "./components/EntryLines";
+import ModalEdit from "./components/ModalEdit";
+import { initialEntries } from "./entries";
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [isExpense, setIsExpense] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [entryId, setEntryId] = useState();
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if (!isOpen && entryId) {
+      const index = entries.findIndex((entry) => entry.id === entryId);
+      const newEntries = [...entries];
+      newEntries[index].description = description;
+      newEntries[index].value = value;
+      newEntries[index].isExpense = isExpense;
+      setEntries(newEntries);
+      resetEntry();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  useEffect(() => {
+    let totalIncomes = 0;
+    let totalExpenses = 0;
+    entries.map((entry) => {
+      if (entry.isExpense) {
+        return (totalExpenses += Number(entry.value));
+      }
+      return (totalIncomes += Number(entry.value));
+    });
+    setTotal(totalIncomes - totalExpenses);
+    setExpenseTotal(totalExpenses);
+    setIncomeTotal(totalIncomes);
+  }, [entries]);
+
+  const deleteEntry = (id) => {
+    console.log("lol");
+    const result = entries.filter((entry) => entry.id !== id);
+    setEntries(result);
+  };
+
+  const addEntry = () => {
+    const result = entries.concat({
+      id: entries.length + 1,
+      description,
+      value,
+      isExpense,
+    });
+    setEntries(result);
+    resetEntry();
+  };
+
+  function editEntry(id) {
+    console.log(`edit entry with id ${id}`);
+    if (id) {
+      const index = entries.findIndex((entry) => entry.id === id);
+      const entry = entries[index];
+      setEntryId(id);
+      setDescription(entry.description);
+      setValue(entry.value);
+      setIsExpense(entry.isExpense);
+      setIsOpen(true);
+    }
+  }
+
+  function resetEntry() {
+    setDescription("");
+    setValue("");
+    setIsExpense(true);
+  }
+
   return (
     <Container>
       <MainHeader title="Budget" />
-
-      <DisplayBalance title="Your Balance:" value="2,550.53" size="small" />
-
-      <DisplayBalances />
-
+      <DisplayBalance title="Your Balance:" value={total} size="small" />
+      <DisplayBalances incomeTotal={incomeTotal} expenseTotal={expenseTotal} />
       <MainHeader title="History" type="h3" />
-      <EntryLine description="income" value="$10.00" />
-      <EntryLine description="expense" value="$10.00" isExpense />
-
+      <EntryLines
+        entries={entries}
+        deleteEntry={deleteEntry}
+        editEntry={editEntry}
+      />
       <MainHeader title="Add new transaction" type="h3" />
-      <NewEntryForm />
+      <NewEntryForm
+        addEntry={addEntry}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
+      <ModalEdit
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
     </Container>
   );
 }
